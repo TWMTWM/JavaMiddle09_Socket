afd2630256cc7835ab0ce83b24d93a05209d271d
TWM <15152806231@163.com>
2020-01-21 10:02:40 +0800

    收发数字

--- src/cn/gtmap/socket_accept_number/Client.java
+++ src/cn/gtmap/socket_accept_number/Client.java
@@ -5,6 +5,12 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * 一旦建立了连接，服务端和客户端就可以通过Socket进行通信了 
+ * 	1. 客户端打开输出流，并发送数字 110 
+ * 	2. 服务端打开输入流，接受数字 110，并打印
+ *
+ */
 public class Client {
 	public static void main(String[] args) {
 		//连接到本机的8888端口
