7f05aa78351c090fc2c129a1d25a5f81a7634c74
TWM <15152806231@163.com>
2020-01-21 09:55:58 +0800

    建立连接

--- src/cn/gtmap/socket/Server.java
+++ src/cn/gtmap/socket/Server.java
@@ -4,6 +4,15 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * 1. 服务端开启8888端口，并监听着，时刻等待着客户端的连接请求 
+ * 2. 客户端知道服务端的ip地址和监听端口号，发出请求到服务端 
+ * 	客户端的端口地址是系统分配的，通常都会大于1024 
+ * 	一旦建立了连接，服务端会得到一个新的Socket对象，该对象负责与客户端进行通信。 
+ * 	注意： 在开发调试的过程中，如果修改过了服务器Server代码，要关闭启动的Server,否则新的Server不能启动，因为8888端口被占用了
+ * @author Administrator
+ *
+ */
 public class Server {
 	public static void main(String[] args) {
 		try {
